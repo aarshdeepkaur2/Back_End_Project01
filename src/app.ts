f795fcc39c79d2bb8fa855053765cff88e49a15a
Arshdeep Kaur <arshdeepkaurr2005@gmail.com>
2025-01-16 12:26:24 -0600

    Implemented and tested the health status of the API.

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express, {Express} from "express"
 
 // importing  swagger
 import setupSwagger from "../config/swagger";
+import { timeStamp } from "console";
 
 // initializing the express application
 const app:  Express = express();
@@ -31,6 +32,31 @@ app.get("/", (req,res) => {
 app.get("/tasks", (req,res)=>{
     res.send("Retriving task")
 });
+
+// example "tasks endpoint"
+/** 
+  * @openapi
+  * /api/v1/health:
+  *  get:
+  *      summary: Get health status of the API
+  *      tags: [Health]
+  *      responses:
+  *          200:
+  *              description: end JSON respone with status, server update, current timestamp and API version   
+*/
+
+app.get("/api/v1/health", (req,res) =>{
+
+    res.json({
+        status:"OK",
+        uptime: process.uptime(), 
+        timestamp: new Date().toISOString(),
+        version: "1.0.0"
+    });
+
+})
+
+
 
 // Testing the server 
 export default app;
--- test/app.test.ts
+++ test/app.test.ts
@@ -16,6 +16,21 @@ describe("GET /", () => {
     });
 });
 
+describe("GET/api/v1/health", ()=> {
+     it("should return the health of the API", async() =>{
+         const response: Response =await request(app).get("/api/v1/health");
+
+         //asset response status to be ok
+         expect(response.status).toBe(200);
+         expect(response.body.status).toBe("OK");
+         expect(response.body).toHaveProperty("uptime");
+         expect(response.body).toHaveProperty("timestamp");
+         expect(response.body).toHaveProperty("version");
+
+
+     })
+    });
+
 
 
 
